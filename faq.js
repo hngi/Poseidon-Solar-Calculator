425697bc3c4fc18d865037a70c6ed43c565b2853
RaphaelNagato <Raphaelnagato19@gmail.com>
2019-09-24 11:24:12 +0100

    add faq section

---
+++ faq.js
@@ -0,0 +1,16 @@
+$(".collapse").on("shown.bs.collapse", function() {
+  $(this)
+    .siblings(".fa-minus-circle")
+    .toggleClass("d-none");
+  $(this)
+    .siblings(".fa-plus-circle")
+    .toggleClass("d-none");
+});
+$(".collapse").on("hidden.bs.collapse", function() {
+  $(this)
+    .siblings(".fa-plus-circle")
+    .toggleClass("d-none");
+  $(this)
+    .siblings(".fa-minus-circle")
+    .toggleClass("d-none");
+});
