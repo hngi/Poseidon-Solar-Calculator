eb6ea6f8418b7a5dc6fe9214cd7d15ea0ab009c0
goodiec <goodnesschrisugari@yahoo.com>
2019-09-24 21:33:33 +0100

    Added the solar calculator section

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+const addBtn = document.querySelector('.add_form button[type=\'submit\']');
+let applianceValue = document.getElementById('appliance_input');
+let quantityValue = document.getElementById('quantity_input');
+let powerValue = document.getElementById('power_input');
+
+const handleAddSubmit = ev => {
+    ev.preventDefault();
+        let newRow = document.createElement('tr');
+        let newAppliance = document.createElement('td');
+        let newQuantity = document.createElement('td');
+        let newPower = document.createElement('td');
+        newAppliance.innerHTML += applianceValue.value;
+        newQuantity.innerHTML += quantityValue.value+ " qty";
+        newPower.innerHTML += powerValue.value+ " watts";
+        newRow.appendChild(newAppliance);
+        newRow.appendChild(newQuantity);
+        newRow.appendChild(newPower);
+        document.querySelector('tbody').appendChild(newRow);
+
+        document.getElementById("calc_form").reset();
+
+}
+
+addBtn.addEventListener('click', handleAddSubmit);
