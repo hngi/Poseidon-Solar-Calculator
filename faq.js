254ff3b890f86422324d3f8ddefc6a0970bb83e8
Umoren Samuel <samuelumoren365@gmail.com>
2019-09-25 20:02:51 +0100

    Merged conflicts

---
+++ faq.js
@@ -0,0 +1,18 @@
+
+$(".collapse").on("shown.bs.collapse", function() {
+  $(this)
+    .siblings(".fa-minus-circle")
+    .toggleClass("d-none");
+  $(this)
+    .siblings(".fa-plus-circle")
+    .toggleClass("d-none");
+});
+$(".collapse").on("hidden.bs.collapse", function() {
+  $(this)
+    .siblings(".fa-plus-circle")
+    .toggleClass("d-none");
+  $(this)
+    .siblings(".fa-minus-circle")
+    .toggleClass("d-none");
+});
+
